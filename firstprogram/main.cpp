296c290927fe0625ccb40f82b34d89e7a3ac52a2
Alex <alex.lucas@focusrite.com>
2013-01-29 20:17:35 +0000

    Initial Commit

---
+++ firstprogram/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  firstprogram
+//
+//  Created by Alex Lucas on 29/01/2013.
+//  Copyright (c) 2013 Alex Lucas. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
