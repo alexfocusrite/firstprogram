0e4354a3314185f757f3427c3e0e34a82d659fbf
Alex <alex.lucas@focusrite.com>
2013-01-29 21:36:27 +0000

    first commit

--- firstprogram/main.cpp
+++ firstprogram/main.cpp
@@ -8,11 +8,71 @@
 
 #include <iostream>
 
-int main(int argc, const char * argv[])
+int main()
 {
+    int num;
+    int error = 1;
+    char choice = 'y';
+    
+    
+    do {
+        std::cout << "\nPlease enter an integer between 0 and 9:\n";
+        
+        do {
+            
+            std::cin >> num;
+            
+            if (num < 0 || num > 9)
+            {
+                std::cout << "You have entered an invalid value please try again:\n";
+                error = 1;
+            }else error = 0;
 
-    // insert code here...
-    std::cout << "Hello, World!\n";
+            
+        } while (error == 1);
+        
+        switch (num)
+        {
+                
+            case 0:
+                std::cout << "Zero\n";
+                break;
+            case 1:
+                std::cout << "One\n";
+                break;
+            case 2:
+                std::cout << "Two\n";
+                break;
+            case 3:
+                std::cout << "Three\n";
+                break;
+            case 4:
+                std::cout << "Four\n";
+                break;
+            case 5:
+                std::cout << "Five\n";
+                break;
+            case 6:
+                std::cout << "Six\n";
+                break;
+            case 7:
+                std::cout << "Seven\n";
+                break;
+            case 8:
+                std::cout << "Eight\n";
+                break;
+            case 9:
+                std::cout << "Nine\n";
+                break;
+        }
+        
+        std::cout << "\nWould you like to run the program again?\n";
+        std::cin >> choice;
+        
+    } while (choice == 'y' || choice == 'Y');
+    
+    std::cout << "Bye!!\n";
+    
     return 0;
 }
 
